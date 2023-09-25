480ff58a3684edd6c4dc175b1de59bea17c39afe
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-09-25 09:43:53 -0300

    fix: esqueleto do projeto

---
+++ server.js
@@ -0,0 +1,5 @@
+import app from "./src/app.js";
+
+app.listen(3005, () => {
+  console.log(`Escutando em: http://localhost:3005`);
+});
---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("deu boa");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).send("deu boa");
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("deu boa");
+});
+export default app;
