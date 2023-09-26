5d47926431042acef27a139b5a530994e555af2b
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-09-26 17:13:48 -0300

    fix: configuração de conecção

--- src/app.js
+++ src/app.js
@@ -1,35 +1,18 @@
 import express from "express";
+import conectaDatabase from "./config/dbConect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("conexão feita com sucesso");
+});
 
 const app = express();
-app.use(express.json());
+routes(app);
 
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node");
-});
-
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("deu boa");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.status(200).send("deu boa");
-});
-
-app.delete("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros.splice(index, 1);
-  res.status(200).send("deu boa");
-});
 export default app;
