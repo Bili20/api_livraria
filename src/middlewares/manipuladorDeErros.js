fc114e0dbfc6cebfbd1e090de6ba94f593b34a7b
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-12 16:59:13 -0300

    Create manipuladorDeErros.js

---
+++ src/middlewares/manipuladorDeErros.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import ErroBase from "../erros/erroBase.js";
+import RequisicaoIncorreta from "../erros/requisicaoIncorreta.js";
+import erroValidacao from "../erros/erroValidacao.js";
+import NaoEncontrado from "../erros/naoEncontrado.js";
+
+function ManipuladorDeErros(erro, req, res, next) {
+  if (erro instanceof mongoose.Error.CastError) {
+    new RequisicaoIncorreta().enviarRespostas(res);
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    new erroValidacao(erro).enviarRespostas(res);
+  } else if (erro instanceof NaoEncontrado) {
+    erro.enviarRespostas(res);
+  } else {
+    new ErroBase().enviarRespostas(res);
+  }
+}
+
+export default ManipuladorDeErros;
