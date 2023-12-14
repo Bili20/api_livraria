c4d460f00bb6dabfb40dd933eb4dfa73e6715919
gabriel milosz cruz <gbrlmilosz@gmail.com>
2023-12-14 08:07:21 -0300

    Update autoresRoutes.js

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import AutorController from "../controllers/autorController.js";
+import Paginar from "../middlewares/paginar.js";
 
 const routes = express.Router();
 
-routes.get("/autores", AutorController.listarAutor);
+routes.get("/autores", AutorController.listarAutor, Paginar);
 routes.get("/autores/:id", AutorController.listarAutorPorId);
 routes.post("/autores", AutorController.cadastrarAutor);
 routes.put("/autores/:id", AutorController.atualizaAutor);
